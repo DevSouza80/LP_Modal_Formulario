783cd110b53b8b9d7d28b77e7868e5568632dbfc
Pedro Henrique Dias de Souza <desenvolvedorweb7070@hotmail.com>
2026-04-24 17:11:16 -0300

    Update modal.js

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -21,4 +21,39 @@ $(function(){
                e.stopPropagation(); 
         });
     }
-})
+
+    $('form#form1').submit(function(){
+        
+       var nome = $('input[name=nome]').val();
+       var telefone = $('input[name=telefone]').val();
+       var email = $('input[name=email]').val();
+         
+       //Contanto a quantidade de espaços e os respectivos valores
+         var amount = nome.split(' ').length;
+         var splitStr = nome.split(' ');
+       if(amount >= 2) {
+           for(var i = 0; i < amount; i++) {
+              if(splitStr[i].match(/[^[A-Z]{1}[a-z]{1,}$/)) {
+
+              } else {
+               aplicarCampoInvalido($('input[name=nome]'));
+                 return false;
+              }
+           }
+       } else {
+               aplicarCampoInvalido($('input[name=nome]'));
+                 return false;
+        return false;
+       }
+
+       return false;
+
+        //Se chegar até o final é por que está tudo okay!
+    });
+
+    function aplicarCampoInvalido(el) {
+                el.css('border','2px solid red');
+                el.data('invalido','true');
+                el.val('Campo Inválido');
+    }
+});
