ec76d6bf779425f386e0729ab29d5de0de41fd43
Pedro Henrique Dias de Souza <desenvolvedorweb7070@hotmail.com>
2026-04-24 11:56:57 -0300

    Update modal.js

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,10 +1,24 @@
 $(function(){
    
     abriJanelaModal();
+    verificarCliqueFechar();
 
     function abriJanelaModal(){
-        $('.btn').click(function(){
-           $('.modal-form').fadeIn();
+        $('.btn').click(function(e){
+              e.stopPropagation(); 
+            $('.modal-form').fadeIn();
+        });
+    }
+
+    function verificarCliqueFechar() {
+        var el = $('body, .close-btn');
+
+        el.click(function(){
+           $('.modal-form').fadeOut();
+        });
+
+        $('.modal-form').click(function(e){
+               e.stopPropagation(); 
         });
     }
 })
